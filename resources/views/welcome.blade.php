54d8edc2db1c6ffc5869c4b54cce33ef55d6c466
Marcio Nacs <marcio.oliveira28@hotmail.com>
2025-03-15 10:43:48 -0300

    Base do projeto

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Marcio Oliveira</title>
+
+
+    </head>
+
+    <body class="">
+
+         <h1>Corpo do Projeto Laravel</h1>
+         <p>Data atual: {{\Carbon\Carbon::now()->format('d/m/y H:i:s')}}</p>
+
+    </body>
+
+</html>
